f70347cdac6cf3911e5b598d481fb99354f97033
CJ-Park <king01286@naver.com>
2025-02-06 10:12:31 +0900

    fix github-actions yaml

--- src/main/java/cj/k8s/app/K8sAppApplication.java
+++ src/main/java/cj/k8s/app/K8sAppApplication.java
@@ -2,8 +2,10 @@ package cj.k8s.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class K8sAppApplication {
 
 	public static void main(String[] args) {
